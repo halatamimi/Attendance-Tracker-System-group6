988a0194b1c469bc97e2c681a91d07bd177439fd
shoroqsalameh <94695708+shoroqsalameh@users.noreply.github.com>
2023-11-25 20:47:10 +0300

    upload js files

---
+++ JS/Add-Traineers.js
@@ -0,0 +1,22 @@
+let email=document.getElementById("email").value;
+let fullName=document.getElementById("fullname").value;
+let password=document.getElementById("password").value;
+let phoneNumber =document.getElementById("phonenumber").value;
+let experience=document.getElementById("experience").value;
+let Addbtn=Document.getElementById("Addbtn");
+
+function createTraineersObject(){
+      let traineersinfo={
+            Email:email,
+            fullName:fullName,
+            password:password,
+            phoneNumber:phoneNumber,
+            experience:experience,
+
+      }
+}
+
+function addTraineers(){
+          
+}
+
